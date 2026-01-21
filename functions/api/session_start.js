54756e17391b8806544d24c5a495969edbbd0a3d
Martinvairo87 <martin.vairo@hotmail.com>
2026-01-21 15:16:30 -0300

    session_start.js

--- functions/api/session_start.js
+++ functions/api/session_start.js
@@ -1,5 +1,6 @@
 export async function onRequest() {
-  return new Response(JSON.stringify({session:"STARTED",ts:Date.now()}),{
-    headers:{'Content-Type':'application/json','Cache-Control':'no-store'}
+  return new Response(JSON.stringify({ session: "STARTED", ts: Date.now() }), {
+    headers: { "content-type": "application/json; charset=utf-8" },
+    status: 200,
   });
 }
