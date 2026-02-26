0779fa39d84ec3a13e70944f8037ae5eb85c757f
Martinvairo87 <martin.vairo@hotmail.com>
2026-02-26 02:18:42 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,43 +1,7 @@
-const CACHE_NAME = "consia-cache-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/voice.html",
-  "/owner.html",
-  "/marketplace.html",
-  "/app.webmanifest"
-];
-
-// Install
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install",e=>{
+ self.skipWaiting();
 });
 
-// Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(names => {
-      return Promise.all(
-        names.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch",e=>{
+ e.respondWith(fetch(e.request));
 });
